d83e5b95e21182f95c02963101c1a11ead119af9
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-06-25 12:49:26 -0400

    Update 3.2_Submit_Sum_1Perc_v3.sh

--- 3_NormalizeTagCount/K10L2/3.2_Submit_Sum_1Perc_v3.sh
+++ 3_NormalizeTagCount/K10L2/3.2_Submit_Sum_1Perc_v3.sh
@@ -1,22 +1,12 @@
-#!/bin/bash
-#SBATCH --job-name=Submit_Sum_1Perc_v3
-#SBATCH --output Submit_Sum_1Perc_v3.%A-%a.out
-#SBATCH --partition=batch
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=mjb51923@uga.edu
-#SBATCH --ntasks=1
-#SBATCH --mem=100gb
-#SBATCH --time=72:00:00
-#SBATCH --array=1-40
-
 #This defines variables
-DIR="/scratch/mjb51923/Ab10_Global_Survey/out"
+DIR=""
 
 #This converts the excel filt to unix
 dos2unix $DIR/Sum1Perc_K10L2_Table.txt
 
 #This pulls info from the array job
 IT=$SLURM_ARRAY_TASK_ID
+#The Sum1Perc_K10L2_Table file is available in this dir under 3.2
 DIRNAME=$(awk NR==${SLURM_ARRAY_TASK_ID}'{print $1}' $DIR/Sum1Perc_K10L2_Table.txt)
 NUM=$(awk NR==${SLURM_ARRAY_TASK_ID}'{print $2}' $DIR/Sum1Perc_K10L2_Table.txt)
 NAME=$(awk NR==${SLURM_ARRAY_TASK_ID}'{print $3}' $DIR/Sum1Perc_K10L2_Table.txt)
@@ -33,4 +23,4 @@ cd $DIR/$DIRNAME/Subset_TaxaFiles
 
 module load R/4.3.1-foss-2022a
 
-Rscript --vanilla /scratch/mjb51923/Ab10_Global_Survey/scripts/Sum_1Perc.R $DIR/$DIRNAME/Subset_TaxaFiles $NAME.$NUM.txt
+Rscript --vanilla Sum_1Perc.R $DIR/$DIRNAME/Subset_TaxaFiles $NAME.$NUM.txt
