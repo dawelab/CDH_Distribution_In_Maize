4e17e9c10ac55350b1e2d8c2f29343c2984ba8bb
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-06-25 13:36:18 -0400

    Update 3.4_Submit_Normalize_SGE_v3.sh

--- 3_NormalizeTagCount/K10L2/3.4_Submit_Normalize_SGE_v3.sh
+++ 3_NormalizeTagCount/K10L2/3.4_Submit_Normalize_SGE_v3.sh
@@ -1,20 +1,6 @@
-#!/usr/bin/bash
-#SBATCH --partition=batch
-#SBATCH -J Submit_Normalize_SGE_v3
-#SBATCH --output Submit_Normalize_SGE_v3.%A-%a.out
-#SBATCH --mem=200GB
-#SBATCH --time=100:00:00
-#SBATCH	--nodes=1
-#SBATCH	--ntasks=12
-#SBATCH --mail-user=meghan.brady@uga.edu
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --array=1-2
-
-#This defines variables
-DIR="/scratch/mjb51923/Ab10_Global_Survey/out"
-
 #This pulls info from the array job
 IT=$SLURM_ARRAY_TASK_ID
+#This table is available under 2.3.2
 DIRNAME=$(awk NR==${SLURM_ARRAY_TASK_ID}'{print $1}' $DIR/Get_K10L2_Table.txt)
 NAME=$(awk NR==${SLURM_ARRAY_TASK_ID}'{print $2}' $DIR/Get_K10L2_Table.txt)
 SGE=$(awk NR==${SLURM_ARRAY_TASK_ID}'{print $3}' $DIR/Get_K10L2_Table.txt)
@@ -31,4 +17,4 @@ echo $SGE
 #Load the modules needed for sapelo2
 module load R/4.3.1-foss-2022a
 
-Rscript --vanilla /scratch/mjb51923/Ab10_Global_Survey/scripts/Normalize_SGE.R $DIR/$DIRNAME $NAME $SGE > /scratch/mjb51923/Ab10_Global_Survey/scripts/Normalize_SGE_$IT.Rout
+Rscript --vanilla Normalize_SGE.R $DIR/$DIRNAME $NAME $SGE > Normalize_SGE_$IT.Rout
