1c8b84a7ffcab056dab90b3cb22231233277d776
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-06-24 20:56:09 -0400

    Update 2.2.1_ID_SGE_Tags_Ab10BChr.sh

--- 2_RunTassel/2.2.1_ID_SGE_Tags_Ab10BChr.sh
+++ 2_RunTassel/2.2.1_ID_SGE_Tags_Ab10BChr.sh
@@ -12,12 +12,12 @@ awk NR==2 $FILE > BChrom.bed
 
 #This identifies only the tag field that overlaps with Ab10 or the B chromosome
 #This bed file is output by TASSEL
-bedtools intersect -wa -a BWAaln_AllData_v5_v_B73-Ab10HIFI_B-Chrom_v2.s.bed -b Ab10.bed > Ab10_TagID.bed
-bedtools intersect -wa -a BWAaln_AllData_v5_v_B73-Ab10HIFI_B-Chrom_v2.s.bed -b BChrom.bed > BChrom_TagID.bed
+bedtools intersect -wa -a BWAaln_AllData_v5_v_B73-Ab10_BChrom.s.bed -b Ab10.bed > Ab10_TagID.bed
+bedtools intersect -wa -a BWAaln_AllData_v5_v_B73-Ab10_BChrom.s.bed -b BChrom.bed > BChrom_TagID.bed
 
 awk '{print $4}' Ab10_TagID.bed > Ab10_TagIDOnly.bed
 awk '{print $4}' BChrom_TagID.bed > BChrom_TagIDOnly.bed
 
 #This gets the Tag only field from the large tag by taxa file 
-awk '{print $1}' Tassel_TagTaxaDist_AllData_v5_v_B73-Ab10HIFI_B-Chrom_v2.txt > Tassel_TagTaxaDist_AllData_v5_v_B73-Ab10HIFI_B-Chrom_v2_TagsOnly.txt
+awk '{print $1}' Tassel_TagTaxaDist_AllData_v5_v_B73-Ab10_BChrom.txt > Tassel_TagTaxaDist_AllData_v5_v_B73-Ab10_BChrom_TagsOnly.txt
 
