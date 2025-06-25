693f0917ed007ed05eda740212518bc93f837853
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-06-25 12:47:39 -0400

    Update 3.2_Submit_Sum_1Perc_v2.sh

--- 3_NormalizeTagCount/Ab10/3.2_Submit_Sum_1Perc_v2.sh
+++ 3_NormalizeTagCount/Ab10/3.2_Submit_Sum_1Perc_v2.sh
@@ -1,19 +1,8 @@
-#!/bin/bash
-#SBATCH --job-name=Submit_Sum_1Perc_v2
-#SBATCH --output Submit_Sum_1Perc_v2.%A-%a.out
-#SBATCH --partition=batch
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=mjb51923@uga.edu
-#SBATCH --ntasks=1
-#SBATCH --mem=100gb
-#SBATCH --time=72:00:00
-#SBATCH --array=1-15
-
 #This defines variables
-DIR="/scratch/mjb51923/Ab10_Global_Survey/out"
+DIR="/"
 
 #This converts the excel filt to unix
-dos2unix $DIR/AlignGBS_HiFiAb10Corrected_v2/Sum1Perc_Table.txt
+dos2unix $DIR/Sum1Perc_Table.txt
 
 #This pulls info from the array job
 IT=$SLURM_ARRAY_TASK_ID
