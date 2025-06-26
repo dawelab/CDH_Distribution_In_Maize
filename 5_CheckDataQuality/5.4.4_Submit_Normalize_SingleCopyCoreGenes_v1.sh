32073efe1b3e722c76b1bc9f8f1998c86a7fa312
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-06-25 21:51:07 -0400

    Update 5.4.4_Submit_Normalize_SingleCopyCoreGenes_v1.sh

--- 5_CheckDataQuality/5.4.4_Submit_Normalize_SingleCopyCoreGenes_v1.sh
+++ 5_CheckDataQuality/5.4.4_Submit_Normalize_SingleCopyCoreGenes_v1.sh
@@ -1,19 +1,7 @@
-#!/usr/bin/bash
-#SBATCH --partition=batch
-#SBATCH -J Submit_Normalize_SingleCopyCoreGenes_v1
-#SBATCH --output Submit_Normalize_SingleCopyCoreGenes_v1.%A-%a.out
-#SBATCH --mem=200GB
-#SBATCH --time=100:00:00
-#SBATCH	--nodes=1
-#SBATCH	--ntasks=12
-#SBATCH --mail-user=meghan.brady@uga.edu
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --array=1-20
-
 #This defines variables
-DIR="/scratch/mjb51923/Ab10_Global_Survey/out"
-DIRNAME="AlignGBS_HiFiAb10Corrected_v2"
-NAME="Tassel_TagTaxaDist_AllData_v5_v_B73-Ab10HIFI_B-Chrom_v2"
+DIR=""
+DIRNAME=""
+NAME="Tassel_TagTaxaDist_AllData_v5_v_B73-Ab10_BChrom"
 IT=$SLURM_ARRAY_TASK_ID
 SGE="SingleCopyCoreGenes"
 
@@ -26,4 +14,4 @@ echo $SGE
 #Load the modules needed for sapelo2
 module load R/4.3.1-foss-2022a
 
-Rscript --vanilla /scratch/mjb51923/Ab10_Global_Survey/scripts/Normalize_SingleCopyCoreGenes.R $DIR/$DIRNAME $NAME $IT $SGE
+Rscript --vanilla Normalize_SingleCopyCoreGenes.R $DIR/$DIRNAME $NAME $IT $SGE
