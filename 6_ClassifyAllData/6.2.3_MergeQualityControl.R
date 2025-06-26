4006fe493c850d80d94cb54dc9ff8e0a08ca8bdf
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-06-26 19:50:17 -0400

    Update 6.2.3_MergeQualityControl.R

--- 6_ClassifyAllData/6.2.3_MergeQualityControl.R
+++ 6_ClassifyAllData/6.2.3_MergeQualityControl.R
@@ -1,7 +1,7 @@
 library(vroom)
 library(data.table)
 
-setwd("/scratch/mjb51923/Ab10_Global_Survey/out/AlignGBS_HiFiAb10Corrected_v2/Ab10_Model/Experimental_Ab10N10Model/QualityControl")
+setwd("/Ab10_Model/Experimental_Ab10N10Model/QualityControl")
 
 #This cbinds all of the sorted columns together
 IT=1
