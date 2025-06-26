6534d926e974baa3d0d6944ecce6e35b6dabb27f
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-06-25 21:04:21 -0400

    Update 5.2.5_Join_Missing_1Perc_K10L2.sh

--- 5_CheckDataQuality/5.2.5_Join_Missing_1Perc_K10L2.sh
+++ 5_CheckDataQuality/5.2.5_Join_Missing_1Perc_K10L2.sh
@@ -1,18 +1,7 @@
-#!/bin/bash
-#SBATCH --job-name=Join_Missing_1Perc_K10L2
-#SBATCH --output Join_Missing_1Perc_K10L2.out
-#SBATCH --partition=batch
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=mjb51923@uga.edu
-#SBATCH --ntasks=1
-#SBATCH --mem=50gb
-#SBATCH --time=1:00:00
-
-
 #This defines variables
-DIR="/scratch/mjb51923/Ab10_Global_Survey/out/"
-DIRNAME="AlignGBS_K10L2Contigs"
-TAXA="Tassel_TagTaxaDist_AllData_v7_v_K10L2Contigs"
+DIR=""
+DIRNAME=""
+TAXA="Tassel_TagTaxaDist_AllData_v7_v_K10L2"
 
 join $DIR/$DIRNAME/Subset_TaxaFiles/$TAXA.Sub1Perc.Missing.15.txt $DIR/$DIRNAME/Subset_TaxaFiles/$TAXA.Sub1Perc.Missing.14.txt > $DIR/$DIRNAME/Subset_TaxaFiles/temp.1
 join $DIR/$DIRNAME/Subset_TaxaFiles/temp.1 $DIR/$DIRNAME/Subset_TaxaFiles/$TAXA.Sub1Perc.Missing.13.txt > $DIR/$DIRNAME/Subset_TaxaFiles/temp.2
