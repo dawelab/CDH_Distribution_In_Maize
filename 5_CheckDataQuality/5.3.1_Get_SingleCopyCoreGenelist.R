e1c6556d9c95e5421581b2cabc52343b875b4dee
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-06-25 21:17:14 -0400

    Update 5.3.1_Get_SingleCopyCoreGenelist.R

--- 5_CheckDataQuality/5.3.1_Get_SingleCopyCoreGenelist.R
+++ 5_CheckDataQuality/5.3.1_Get_SingleCopyCoreGenelist.R
@@ -1,3 +1,4 @@
-CORE <- read.csv("/Users/user/University_of_Georgia/Dawe_Lab_Documents/Ab10_Global_Survey/Data/single_core_B73v5.csv")
+#This file is a filtered version of https://raw.githubusercontent.com/dawelab/Natural-methylation-epialleles-correlate-with-gene-expression-in-maize/main/Data/B73.all.csv. It contains only genes that are single copy across the pangenome. 
+CORE <- read.csv("single_core_B73v5.csv")
 
 write.table(CORE$gene, file="single_core_B73v5_genename.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
