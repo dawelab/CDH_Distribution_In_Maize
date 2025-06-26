cb1d4b445d9df280fc2422ab8443ea67df19e918
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-06-25 21:59:48 -0400

    Update 5.4.6_Submit_SCG_DataFiltering_RNLineMerging_v2.sh

--- 5_CheckDataQuality/5.4.6_Submit_SCG_DataFiltering_RNLineMerging_v2.sh
+++ 5_CheckDataQuality/5.4.6_Submit_SCG_DataFiltering_RNLineMerging_v2.sh
@@ -1,15 +1,3 @@
-#!/usr/bin/bash
-#SBATCH --partition=batch
-#SBATCH -J Submit_SCG_DataFiltering_RNLineMerging_v2
-#SBATCH --output Submit_SCG_DataFiltering_RNLineMerging_v2.%A-%a.out
-#SBATCH --mem=100GB
-#SBATCH --time=24:00:00
-#SBATCH	--nodes=1
-#SBATCH	--ntasks=12
-#SBATCH --mail-user=meghan.brady@uga.edu
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --array=1-20
-
 #This defines variables
 IT=$SLURM_ARRAY_TASK_ID
 
@@ -20,4 +8,4 @@ echo $IT
 #Load the modules needed for sapelo2
 module load R/4.3.1-foss-2022a
 
-Rscript --vanilla /scratch/mjb51923/Ab10_Global_Survey/scripts/SCG_DataFiltering_RNLineMerging_v2.R $IT
+Rscript --vanilla SCG_DataFiltering_RNLineMerging_v2.R $IT
