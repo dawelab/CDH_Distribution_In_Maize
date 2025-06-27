9b4b23579cdc549db667586bd9b0d6cb942e3da2
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-06-26 20:29:26 -0400

    Update 6.3.2_Submit_K10L2_KMeansModel_MixedControls_v2.sh

--- 6_ClassifyAllData/6.3.2_Submit_K10L2_KMeansModel_MixedControls_v2.sh
+++ 6_ClassifyAllData/6.3.2_Submit_K10L2_KMeansModel_MixedControls_v2.sh
@@ -1,15 +1,3 @@
-#!/usr/bin/bash
-#SBATCH --partition=batch
-#SBATCH -J Submit_K10L2_KMeansModel_MixedControls_v2
-#SBATCH --output Submit_K10L2_KMeansModel_MixedControls_v2.%A-%a.out
-#SBATCH --mem=30GB
-#SBATCH --time=10:00:00
-#SBATCH	--nodes=1
-#SBATCH	--ntasks=1
-#SBATCH --mail-user=meghan.brady@uga.edu
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --array=1-25
-
 #This defines the variables
 IT=$SLURM_ARRAY_TASK_ID
 
@@ -20,4 +8,4 @@ echo $IT
 #Load the modules needed for sapelo2
 module load R/4.3.1-foss-2022a
 
-Rscript --vanilla /scratch/mjb51923/Ab10_Global_Survey/scripts/K10L2_KMeansModel_MixedControls_v2.R $IT
+Rscript --vanilla K10L2_KMeansModel_MixedControls_v2.R $IT
