4f611e63d9d9c0a827eb6e9c6b82bcd849381cbb
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-06-27 15:14:38 -0400

    Update 3_TASSEL_GWAS_Mo17_K10L2.sh

--- 7_GWAS/Call_SNPs/3_TASSEL_GWAS_Mo17_K10L2.sh
+++ 7_GWAS/Call_SNPs/3_TASSEL_GWAS_Mo17_K10L2.sh
@@ -1,25 +1,14 @@
-#!/bin/bash
-#SBATCH --partition=highmem_p
-#SBATCH -J TASSEL_GWAS_B73v3_K10L2
-#SBATCH --output TASSEL_GWAS_B73v3_K10L2.out
-#SBATCH --mem=900GB
-#SBATCH --time=48:00:00
-#SBATCH	--nodes=1
-#SBATCH	--ntasks=30
-#SBATCH --mail-user=meghan.brady@uga.edu
-#SBATCH --mail-type=END
-
-#Load modules
+Load modules
 module load TASSEL/5.2.44-Java-1.8.0_241
 module load BWA/0.7.17-GCCcore-11.3.0
 module load SAMtools/0.1.20-GCC-11.3.0
 module load BEDTools/2.29.2-GCC-8.3.0
 
 #Define Variables
-OUT_DIR="/scratch/mjb51923/Ab10_Global_Survey/out/AlignGBS_HiFiAb10Corrected_v2"
-READ_DIR="/scratch/mjb51923/raw_reads/GBS/Swarts_AllControls_LengthFiltFakeBarcodedRomeroNavarro_Romay"
-KEY="/scratch/mjb51923/Ab10_Global_Survey/Ab10-Global-Survey/8_SNPs/SwartsAllControlsLengthFiltRomeroNavarroRomay_Key_GWASSNPs.txt"
-REF="/scratch/mjb51923/Ab10_Global_Survey/out/AlignGBS_HiFiAb10Corrected_v2/Zm-Mo17-REFERENCE-CAU-2.0_K10L2.fa"
+OUT_DIR=""
+READ_DIR="Swarts_AllControls_LengthFiltFakeBarcodedRomeroNavarro_Romay"
+KEY="SwartsAllControlsLengthFiltRomeroNavarroRomay_Key_GWASSNPs.txt"
+REF="Zm-Mo17-REFERENCE-CAU-2.0_K10L2.fa"
 NAME="GWAS_Mo17_K10L2"
 
 #This v2 refers to the corrected Hifi genome
