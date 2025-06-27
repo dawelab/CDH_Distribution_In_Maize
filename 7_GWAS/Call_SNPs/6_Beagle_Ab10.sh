e5a7e015b9bdda9ef534e373218576bf974b95ce
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-06-27 15:19:43 -0400

    Update 6_Beagle_Ab10.sh

--- 7_GWAS/Call_SNPs/6_Beagle_Ab10.sh
+++ 7_GWAS/Call_SNPs/6_Beagle_Ab10.sh
@@ -1,15 +1,3 @@
-#!/usr/bin/bash
-#SBATCH --partition=batch
-#SBATCH -J Beagle_Ab10
-#SBATCH --output Beagle_Ab10_try2.%A-%a.out
-#SBATCH --mem=100GB
-#SBATCH --time=5:00:00
-#SBATCH	--nodes=1
-#SBATCH	--ntasks=24
-#SBATCH --mail-user=meghan.brady@uga.edu
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --array=1-10
-
 #This defines the variables
 IT=$SLURM_ARRAY_TASK_ID
 
@@ -18,7 +6,7 @@ module load Beagle/5.4.22Jul22.46e-Java-11
 module load BCFtools/1.15.1-GCC-11.3.0
 
 #Define variables
-DIR="/scratch/mjb51923/Ab10_Global_Survey/out/AlignGBS_HiFiAb10Corrected_v2"
+DIR=""
 VCF="GWAS_Mo17_Ab10.chr.landrace.filt"
 CDH="AB10"
 
@@ -28,7 +16,7 @@ CHR=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
 chr=${CHR[$IT-1]}
 echo "############ working on chr"$chr
 
-#This performs imputation for beagle. I am using an effective population size of 1000 because that was the best estimate from the bottle neck
+#This performs imputation for beagle. I am using an effective population size of 1000 for the domestication bottle neck
 java -Xmx90g -jar /apps/eb/Beagle/5.4.22Jul22.46e-Java-11/beagle.jar \
 gt=$DIR/${VCF}_chr${chr}.vcf.gz \
 out=$DIR/${VCF}_chr${chr}.imputed \
